7739cade7daa64a6aefd1fa627cb1b23d61ce761
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-14 16:13:32 +0200

    editor ready

--- app/src/main/java/com/example/shopping_list/ShoppingList.kt
+++ app/src/main/java/com/example/shopping_list/ShoppingList.kt
@@ -1,16 +1,27 @@
 package com.example.shopping_list
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(val id: Int, var name: String, var quantity: Int, var isEditing: Boolean = false)
@@ -49,7 +61,7 @@ fun ShoppingListApp(){
                 .padding(16.dp)
         ) {
             items(sItems){
-
+                ShoppingListItem(it, {},{})
             }
         }
     }
@@ -93,5 +105,55 @@ fun ShoppingListApp(){
             }
         )
 
+    }
+}
+
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit){
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.White)
+        .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly){
+        Column{
+            BasicTextField(value = editedName, onValueChange = {editedName = it}, singleLine = true, modifier = Modifier
+                .wrapContentSize()
+                .padding(8.dp))
+            BasicTextField(value = editedQuantity, onValueChange = {editedQuantity = it}, singleLine = true, modifier = Modifier
+                .wrapContentSize()
+                .padding(8.dp))
+        }
+
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+        } ){
+            Text(text = "Save")
+        }
+    }
+
+}
+@Composable
+fun ShoppingListItem(item: ShoppingItem, onEditClick: () -> Unit, onDeleteClick:() -> Unit ){
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(border = BorderStroke(2.dp, Color.Magenta), shape = RoundedCornerShape(20))
+    ){
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.padding(8.dp)){
+            IconButton(onClick = onEditClick){
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick){
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
     }
 }
